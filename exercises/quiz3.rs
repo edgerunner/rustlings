174ca7eabb21b46233d1748644000b05d236ce5e
Mert Torun <mert@merttorun.com>
2023-06-13 20:38:43 +0000

    Done with generics & traits

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,15 +16,18 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
-pub struct ReportCard {
-    pub grade: f32,
+
+use std::fmt::Display;
+
+pub struct ReportCard<'g> {
+    pub grade: &'g dyn Display,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl <'g> ReportCard<'_> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -38,7 +41,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: &2.1,
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -52,7 +55,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: &"A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
